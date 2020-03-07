d87a3fffa0b070795b2917f98c45b524652cb324
andros2x21 <47262174+andros2x21@users.noreply.github.com>
2020-03-07 01:57:05 +0100

    dodata klasa obavjestenje

--- korisnik/admin.py
+++ korisnik/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # Register your models here.
-from .models import Korisnik,Stranica
+from .models import Korisnik,Stranica,Obavjestenje
 
 admin.site.register(Korisnik)
 admin.site.register(Stranica)
+admin.site.register(Obavjestenje)
--- korisnik/models.py
+++ korisnik/models.py
@@ -18,3 +18,9 @@ class Stranica(models.Model):
     korisnik = models.ForeignKey(Korisnik,on_delete=models.CASCADE)
     def __str__(self):
         return "{}".format(self.link)
+DEFAULT_EXAM_ID = 1
+class Obavjestenje(models.Model):
+    korisnik = models.ForeignKey(Korisnik, on_delete=models.CASCADE)
+    naziv = models.CharField(max_length=20)
+    sadrzaj = models.CharField(max_length=20)
+    pub_date = models.DateTimeField('vrijeme promjene')
