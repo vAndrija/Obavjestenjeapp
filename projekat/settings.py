fdfabf0e9036a4d56825596c1b08ef33a8fca7c8
andros2x21 <47262174+andros2x21@users.noreply.github.com>
2020-03-19 15:56:42 +0100

    poruka

--- projekat/settings.py
+++ projekat/settings.py
@@ -131,4 +131,5 @@ STATIC_URL = '/static/'
 django_heroku.settings(locals())
 
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
