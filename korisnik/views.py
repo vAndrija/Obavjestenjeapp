4ee45290cfd5371039c1f931ebf4cb8b550c7de8
andros2x21 <47262174+andros2x21@users.noreply.github.com>
2020-04-01 22:46:22 +0200

    dodate promjene

--- korisnik/views.py
+++ korisnik/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
 import urllib.request
+from background_task import models
 from background_task import background
 from bs4 import BeautifulSoup,Comment
 from django.core.mail import send_mail
@@ -17,9 +18,34 @@ def tag_visible(element):
         return False
     return True
 
+def provjera_postojanja(link, username):
 
-@background(schedule=10)
+    for task in models.Task.objects.all():
+        text = "{}".format(task.task_params)
+        odvojeni = text.split(",")
+        stranica = odvojeni[0].split('"')
+        korisnik = odvojeni[1].split('"')
+        if(username==korisnik[1] and link==stranica[1]):
+            try:
+                vlasnik = Korisnik.objects.get(username =korisnik[1])
+                try:
+                    nadjenaStranica = vlasnik.stranica_set.get(link=stranica[1])
+                except:
+                    print("Treba da se izbrise")
+                    task.delete()
+                    return False
+            except:
+                return False
+
+
+    return True
+
+@background(schedule=20)
 def obavjestenje(link,username):
+    povratna = provjera_postojanja(link,username)
+    if(not povratna):
+        return
+    print("Nit je prosla sa {}".format(link))
     try:
         html = urllib.request.urlopen(link)
     except:
@@ -61,7 +87,7 @@ def adding(request,username):
     except:
         pass
     objekat.stranica_set.create(link = request.POST['link'])
-    obavjestenje(request.POST['link'],username, schedule=60,repeat=10)
+    obavjestenje(request.POST['link'],username,repeat=40)
 
     return HttpResponseRedirect(reverse('korisnik:logged',args=(username,)))
 
@@ -81,6 +107,8 @@ def delatingLink(request,username):
     objekat = Stranica.objects.get(link=link)
     objekat.delete()
     return HttpResponseRedirect(reverse('korisnik:logged',args=(username,)))
+
+
 def help(request):
     try:
         username = request.POST['username']
@@ -89,9 +117,8 @@ def help(request):
             return HttpResponseRedirect(reverse('korisnik:home'))
     except(KeyError, Korisnik.DoesNotExist):
         return HttpResponseRedirect(reverse('korisnik:home'))
-
-
     return HttpResponseRedirect(reverse('korisnik:logged',args=(object.username,)))
+
 
 def logged(request,username):
     objekat = Korisnik.objects.get(username =username)
@@ -101,9 +128,10 @@ def logged(request,username):
         return HttpResponseRedirect(reverse('korisnik:home'))
 
 
-
+    lista  = objekat.obavjestenje_set.all()[0:5]
     return render(request,'korisnik/index.html',{'username':username,
-                                                   'objekat':objekat})
+                                                   'objekat':objekat,
+                                                 'obavjestenja': lista})
 
 def register(request):
     return render(request,"korisnik/registrationa.html",{})
